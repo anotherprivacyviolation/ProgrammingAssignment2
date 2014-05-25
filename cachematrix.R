24e35e26753e180e74337e3a510a751ca625708c
anotherprivacyviolation <wuxiaoxin@rogers.com>
2014-05-25 18:28:02 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+mmakeCacheMatrix <- function(x = matrix()) {
+        xInv <- NULL
+        setx <- function(y) {
+                x <<- y
+                xInv <<- NULL
+        }
+        getx <- function() x
+        setxInv <- function(I) xInv <<- I
+        getxInv <- function() xInv
+        list(setx = setx, getx = getx,
+             setxInv = setxInv,
+             getxInv = getxInv)
+
 
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        xInv <- x$getxInv()
+        if(!is.null(xInv)) {
+                message("getting cached data")
+                return(xInv)
+        }
+        data <- x$getx()
+        xInv <- solve(data, ...)
+        x$setxInv(xInv)
+        xInv
+
 }
